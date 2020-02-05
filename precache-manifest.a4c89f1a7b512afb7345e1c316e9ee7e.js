710fbc0b14ac23b599f85fe805e62fe469645ede
صفوان القليطي <safwanalqulayti@gmail.com>
2020-02-05 21:21:31 +0300

    Updates

---
+++ precache-manifest.a4c89f1a7b512afb7345e1c316e9ee7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5c1143845658a05ced3326ae15c63f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "202ee59f35c79bcc889d",
+    "url": "/my-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9616d3cd196665a58ed6",
+    "url": "/my-app/static/css/main.ade02319.chunk.css"
+  },
+  {
+    "revision": "202ee59f35c79bcc889d",
+    "url": "/my-app/static/js/2.a29d6c5a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/my-app/static/js/2.a29d6c5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9616d3cd196665a58ed6",
+    "url": "/my-app/static/js/main.1d0260fb.chunk.js"
+  },
+  {
+    "revision": "a50e9d91e2a568008273",
+    "url": "/my-app/static/js/runtime-main.2d8e6340.js"
+  },
+  {
+    "revision": "46aaeee7f3affabfbd7c7c699c8db813",
+    "url": "/my-app/static/media/logo.46aaeee7.jpg"
+  }
+]);
