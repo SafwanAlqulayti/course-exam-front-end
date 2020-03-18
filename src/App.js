19541560a1b1a8acb8a990443a20036b9dc7c6b5
صفوان القليطي <safwanalqulayti@gmail.com>
2020-03-18 03:39:12 +0300

    test routing

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App(props) {
         </ul> */}
         {/* <MyInfo use rInfo={props.useID}/> */}
 <Header></Header>
-            <Route exact path= {path +"/" }    component={Home} />
+            <Route exact path=  "/"     component={Home} />
          <Route path= "/login" component={Login} />
             <Route path="/myinfo" component={MyInfo} />
             <Route path="/thequiz" component={TheQuiz} />
